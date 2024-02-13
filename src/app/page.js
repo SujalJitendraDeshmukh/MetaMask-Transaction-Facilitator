9974262327f4070884a02e279821fc74cbbc1164
Shashwat Singh <shashwat123student@gmail.com>
2024-02-13 18:13:43 +0530

    Fixed the providers error and dotenv

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@ import {Button} from "@mui/material";
 import { ethers } from "ethers";
 import fs from "fs";
 import path from "path";
-require("dotenv").config();
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default function Home() {
-    const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-    const compiledContract = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./Abi/contracts_TransferTokens_sol_TransferTokens.abi"), "utf8"));
-    const contractABI = compiledContract;
+    console.log(__dirname);
+    const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+    // const compiledContract = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    // const contractABI = compiledContract.abi;
     const contractAddress = "0x3072e8A9eB5fa30ac52BF12e8C2eBF5f09178649";
     const provider = new ethers.providers.JsonRpcProvider(SEPOLIA_RPC_URL);
     const signer = provider.getSigner("0xfB43419929FA1DeDF67B51252Eb734844c3f52D0")
