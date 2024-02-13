38cffc2551cf5a991910d253b2a48294af2e448e
Shashwat Singh <shashwat123student@gmail.com>
2024-02-13 13:48:08 +0530

    going to class

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@ import {Button} from "@mui/material";
 import { ethers } from "ethers";
 import fs from "fs";
 import path from "path";
+require("dotenv").config();
 
 export default function Home() {
-    const contractABI = [
-        "function transferEther(address payable _to, uint256 _amount) external payable"
-    ];
-    const contractAddress = "YOUR_CONTRACT_ADDRESS";
-    const provider = new ethers.Provider.JsonRpcProvider("YOUR_JSON_RPC_PROVIDER_URL");
+    const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+    const compiledContract = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./Abi/contracts_TransferTokens_sol_TransferTokens.abi"), "utf8"));
+    const contractABI = compiledContract;
+    const contractAddress = "0x3072e8A9eB5fa30ac52BF12e8C2eBF5f09178649";
+    const provider = new ethers.providers.JsonRpcProvider(SEPOLIA_RPC_URL);
+    const signer = provider.getSigner("0xfB43419929FA1DeDF67B51252Eb734844c3f52D0")
 
   return (
       <div>
