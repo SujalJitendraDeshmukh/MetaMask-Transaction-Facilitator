f556d16eff120ecc155df8900e0de51ef357ccb6
Shashwat Singh <shashwat123student@gmail.com>
2024-02-13 18:34:44 +0530

    Block Number Working

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,32 @@
+"use client"
+
 import {Button} from "@mui/material";
 import { ethers } from "ethers";
-import fs from "fs";
-import path from "path";
 import dotenv from "dotenv";
+import {useEffect, useState} from "react";
 
 dotenv.config();
 
 export default function Home() {
+    const [blockNumber, setBlockNumber] = useState(null);
+
     console.log(__dirname);
-    const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
     // const compiledContract = JSON.parse(fs.readFileSync(filePath, "utf8"));
     // const contractABI = compiledContract.abi;
     const contractAddress = "0x3072e8A9eB5fa30ac52BF12e8C2eBF5f09178649";
-    const provider = new ethers.providers.JsonRpcProvider(SEPOLIA_RPC_URL);
+    const provider = new ethers.providers.JsonRpcProvider("https://eth-sepolia.g.alchemy.com/v2/WBdGDtGfarFaZsi7KwpVLBJWSICKBol4");
     const signer = provider.getSigner("0xfB43419929FA1DeDF67B51252Eb734844c3f52D0")
+
+        const fetchBlockNumber = async () => {
+            try {
+                const num = await provider.getBlockNumber();
+                setBlockNumber(num);
+            } catch (error) {
+                console.error('Error fetching block number:', error);
+            }
+    }
+
+
 
   return (
       <div>
@@ -24,6 +37,11 @@ export default function Home() {
             <input type={"number"} placeholder={"Enter the Amount"}/>
               <Button variant="text">Transfer</Button>
           </form>
+          <div>
+              <h1>This is gonna be the block number</h1>
+              <Button onClick={fetchBlockNumber}>Set Block Number</Button>
+              {blockNumber}
+          </div>
       </div>
   );
 }
