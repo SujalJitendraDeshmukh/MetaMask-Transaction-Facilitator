2867f5aeeed59f2df7ca33f549c5fac08b264d2e
Tanish Bhole <tanishbhole@gmail.com>
2024-02-14 16:31:19 +0530

    added Connection to Metamask

--- src/app/page.js
+++ src/app/page.js
@@ -1,70 +1,117 @@
-"use client"
+"use client";
 
 import { Button } from "@mui/material";
-import { useState } from "react";
-import TransferTokensAbi from '@/assets/web3/Abis/TransferTokensAbi.json';
+import { useState, useEffect } from "react";
+import TransferTokensAbi from "@/assets/web3/Abis/TransferTokensAbi.json";
 import { AlchemySepoliaProvider } from "@/assets/web3/Provider";
 import { TokenTransferContractAddress } from "@/assets/web3/Address";
 import { ethers } from "ethers";
 
 export default function Home() {
-    const [blockNumber, setBlockNumber] = useState(null);
-    const [name, setName] = useState(null);
-    const [txHash, setTxHash] = useState(null);
+  const [blockNumber, setBlockNumber] = useState(null);
+  const [name, setName] = useState(null);
+  const [txHash, setTxHash] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  //   const provider = new ethers.providers.JsonRpcProvider(AlchemySepoliaProvider);
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //set signer after getting the account id from meta mask
 
-    const provider = new ethers.providers.JsonRpcProvider(AlchemySepoliaProvider);
-    const signer = provider.getSigner("0xfB43419929FA1DeDF67B51252Eb734844c3f52D0")
-    console.log(signer);
+  const connectMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    const TransferTokenContract = new ethers.Contract(TokenTransferContractAddress, TransferTokensAbi, provider);
-
-    const fetchBlockNumber = async () => {
-        try {
-            const num = await provider.getBlockNumber();
-            setBlockNumber(num);
-        } catch (error) {
-            console.error('Error fetching block number:', error);
-        }
+        setIsConnected(true);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("MetaMask extension not detected!");
     }
+  };
 
-    const setMyName = async () => {
-        try {
-            const myName = await TransferTokenContract.check();
-            setName(myName);
-        } catch (error) {
-            console.error('Error fetching name:', error);
-        }
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", () => {
+        window.location.reload();
+      });
     }
+  }, []);
 
-    const transferEther = async (to, amount) => {
-        try {
-            const ttiwthSigner = TransferTokenContract.connect(signer);
-            const Wei = ethers.utils.parseUnits("1.0", 18);
-            const tx = ttiwthSigner.transferEther(to,amount);
-            setTxHash(tx);
-        } catch (error) {
-            console.error("Error transferring ether:", error);
-        }
-    };
+  const TransferTokenContract = new ethers.Contract(
+    TokenTransferContractAddress,
+    TransferTokensAbi,
+    provider
+  );
 
-    return (
-        <div>
-            <h1>Hey this is the heading</h1>
-            <form>
-                <input type={"text"} placeholder={"Enter Recipient's Address"} />
-                <input type={"number"} placeholder={"Enter the Amount"} />
-            </form>
-            <div>
-                <h1>This is gonna be the block number</h1>
-                <Button onClick={fetchBlockNumber}>Set Block Number</Button>
-                {blockNumber}
-                <h1>This is gonna be my name</h1>
-                <Button onClick={setMyName}>Fetch Name</Button>
-                {name}
-                <h1>Send Transaction</h1>
-                <Button variant="text" onClick={() => transferEther("0xd8A6BFc168C67c56173E6EbA194fD0eB9E0e5D39", 1)}>Transfer</Button>
-                {txHash}
-            </div>
-        </div>
-    );
+  const fetchBlockNumber = async () => {
+    try {
+      const num = await provider.getBlockNumber();
+      setBlockNumber(num);
+    } catch (error) {
+      console.error("Error fetching block number:", error);
+    }
+  };
+
+  const setMyName = async () => {
+    try {
+      const myName = await TransferTokenContract.check();
+      setName(myName);
+    } catch (error) {
+      console.error("Error fetching name:", error);
+    }
+  };
+
+  const transferEther = async (to, amount) => {
+    try {
+      const signer = provider.getSigner(account);
+      const ttiwthSigner = TransferTokenContract.connect(signer);
+      const Wei = ethers.utils.parseUnits("1.0", 18);
+      const tx = ttiwthSigner.transferEther(to, amount);
+      setTxHash(tx);
+    } catch (error) {
+      console.error("Error transferring ether:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hey this is the heading</h1>
+      <form>
+        <input type={"text"} placeholder={"Enter Recipient's Address"} />
+        <input type={"number"} placeholder={"Enter the Amount"} />
+      </form>
+      <div>
+        <h1>This is gonna be the block number</h1>
+        <Button onClick={fetchBlockNumber}>Set Block Number</Button>
+        {blockNumber}
+        <h1>This is gonna be my name</h1>
+        <Button onClick={setMyName}>Fetch Name</Button>
+        {name}
+        <h1>Send Transaction</h1>
+        <Button
+          variant="text"
+          onClick={() =>
+            transferEther("0xd8A6BFc168C67c56173E6EbA194fD0eB9E0e5D39", 1)
+          }
+        >
+          Transfer
+        </Button>
+        {txHash}
+        {!isConnected ? (
+          <button onClick={connectMetaMask}>Connect MetaMask</button>
+        ) : (
+          <div>
+            <p>Connected with MetaMask</p>
+            <p>Account: {account}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
