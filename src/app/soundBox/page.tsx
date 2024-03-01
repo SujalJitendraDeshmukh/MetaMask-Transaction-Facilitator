f9a10f145a341f623550c5c3dfa53e7872a3c8d4
Shashwat Singh <shashwat123student@gmail.com>
2024-03-01 18:11:51 +0530

    SoundCloud is working

--- src/app/soundBox/page.tsx
+++ src/app/soundBox/page.tsx
@@ -1,53 +1,54 @@
-"use client";
+'use client';
 
 import { useDispatch, useSelector } from 'react-redux';
-import {useEffect, useState} from "react";
-import {formatEther} from "ethers/lib/utils";
-import {Button} from "@mui/material";
+import { useEffect, useState } from "react";
+import { formatEther } from "ethers/lib/utils";
+import { Button } from "@mui/material";
 import toTransferPolygon from "@/hooks/toTransferPolygon";
-import {RootState} from "@/provider/redux/store";
+import { RootState } from "@/provider/redux/store";
 import toTransferETH from "@/hooks/toTransferETH";
 import toTransferBSC from "@/hooks/toTransferBSC";
 import toTransferAVN from "@/hooks/toTransferAVN";
 import Link from "next/link";
-import {UserButton} from "@clerk/nextjs";
-export default function soundBox(){
+import { UserButton } from "@clerk/nextjs";
 
+export default function SoundBox() {
     const type = useSelector((state: RootState) => state.SetBlockchain.name);
     const address = useSelector((state: RootState) => state.SetAccount.name);
 
-    const { getContractETH } = toTransferETH();
-    const { getContractBSC } = toTransferBSC();
-    const { getContractPolygon } = toTransferPolygon();
-    const { getContractAVN } = toTransferAVN();
-
-
-    let TransferTokenContract;
     const [filteredData, setFilteredData] = useState("");
 
-    useEffect(()=>{
-        if(type === 'ETH'){
-            TransferTokenContract = getContractETH();
-        } else if( type === 'BSC' ){
-            TransferTokenContract = getContractBSC();
-        } else if( type === 'AVL'){
-            TransferTokenContract = getContractAVN();
-        } else if( type === 'PLG'){
-            TransferTokenContract = getContractPolygon()
+    useEffect(() => {
+        if (type === 'ETH') {
+            const { getContractETH } = toTransferETH();
+            const TransferTokenContract = getContractETH();
+            const filter = TransferTokenContract.filters.Transfer(null, address);
+            TransferTokenContract.on(filter, (_from,_to,_amount,Event)=>{
+                setFilteredData(`${_from} sent ${formatEther(_amount)} to ${_to}`);
+            })
+        } else if (type === 'BSC') {
+            const { getContractBSC } = toTransferBSC();
+            const TransferTokenContract = getContractBSC();
+            const filter = TransferTokenContract.filters.Transfer(null, address);
+            TransferTokenContract.on(filter, (_from,_to,_amount,Event)=>{
+                setFilteredData(`${_from} sent ${formatEther(_amount)} to ${_to}`);
+            })
+        } else if (type === 'AVL') {
+            const { getContractAVN } = toTransferAVN();
+            const TransferTokenContract = getContractAVN();
+            const filter = TransferTokenContract.filters.Transfer(null, address);
+            TransferTokenContract.on(filter, (_from,_to,_amount,Event)=>{
+                setFilteredData(`${_from} sent ${formatEther(_amount)} to ${_to}`);
+            })
+        } else if (type === 'PLG') {
+            const { getContractPolygon } = toTransferPolygon();
+            const TransferTokenContract = getContractPolygon();
+            const filter = TransferTokenContract.filters.Transfer(null, address);
+            TransferTokenContract.on(filter, (_from,_to,_amount,Event)=>{
+                setFilteredData(`${_from} sent ${formatEther(_amount)} to ${_to}`);
+            })
         }
-    },[type])
-    // const { getContractPolygon } = toTransferPolygon();
-    // const TransferTokenContract = getContract();
-    // TransferTokenContract = getContractPolygon();
-    const filter = TransferTokenContract.filters.Transfer(null, address);
-
-    // TransferTokenContract.on("Transfer", (_from,_to,_amount,Event)=>{
-    //     setData(`${_from} sent ${formatEther(_amount)} to ${_to}`);
-    // })
-
-    TransferTokenContract.on(filter, (_from,_to,_amount,Event)=>{
-        setFilteredData(`${_from} sent ${formatEther(_amount)} to ${_to}`);
-    })
+    }, [type, address]);
 
     useEffect(() => {
         if (filteredData) {
@@ -55,7 +56,7 @@ export default function soundBox(){
         }
     }, [filteredData]);
 
-    function speak(text) {
+    function speak(text : string) {
         const utterance = new SpeechSynthesisUtterance(text);
         const voices = speechSynthesis.getVoices();
         utterance.voice = voices[0];
@@ -64,11 +65,6 @@ export default function soundBox(){
 
     return(
         <div>
-            <Link href="/dashboard">Dashbaord</Link>
-            <Link href="/">LandingPage</Link>
-            <Link href="/soundBox">SoundBox</Link>
-            <Link href="/transfer">Transfer</Link>
-            <UserButton></UserButton>
             <h1>Only those events are captured whose address is there</h1>
             <p>{filteredData}</p>
         </div>
